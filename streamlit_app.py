90402c129cd007d256419958e7e220ff1784393e
iamabhijithv <78366676+iamabhijithv@users.noreply.github.com>
2024-10-02 18:00:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,3 +43,8 @@ if ingredients_list:
         st.success('Your Smoothie is ordered! '+ name_on_order, icon="✅")
 
 # if ingredients_list is not null: then do everything below this line that is indented.
+
+# new section to display fruitvise nutrition information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
